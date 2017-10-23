8d2dad422aecf587ecb475d52c6cdc5011431e8f
Shakhzod Ikromov <aabbcc.double@gmail.com>
2017-10-23 04:48:05 +0300

    Initial commit

---
+++ carthage-bug-framework/CarthageBugFramework.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for carthage_bug_framework.
+FOUNDATION_EXPORT double CarthageBugFrameworkVersionNumber;
+
+//! Project version string for carthage_bug_framework.
+FOUNDATION_EXPORT const unsigned char CarthageBugFrameworkVersionString[];
+
